21d3e8007c7903b2d1496e6298c7048960e5c346
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-08 15:35:57 +0100

    feat: styling css

--- js/myscript.js
+++ js/myscript.js
@@ -8,18 +8,22 @@ for (let counter = 1; counter <= 100; counter++) {
         console.log("FizzBuzz");
         FizzBuzzElement.append("FizzBuzz");
         FizzBuzzList.append(FizzBuzzElement);
+        FizzBuzzElement.classList.add("purple");
     } else if (counter % 3 === 0) {
         console.log("Fizz");
         FizzBuzzElement.append("Fizz");
         FizzBuzzList.append(FizzBuzzElement);
+        FizzBuzzElement.classList.add("green");
     } else if (counter % 5 === 0) {
         console.log("Buzz");
         FizzBuzzElement.append("Buzz");
         FizzBuzzList.append(FizzBuzzElement);
+        FizzBuzzElement.classList.add("gold");
     } else {
         console.log(counter);
         FizzBuzzElement.append(counter);
         FizzBuzzList.append(FizzBuzzElement);
+        FizzBuzzElement.classList.add("grey");
     }
 }
 
