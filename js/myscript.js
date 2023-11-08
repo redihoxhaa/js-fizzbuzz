f36c82c7b1ccc50d56eebfd3591131e9175be2ed
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-08 15:18:40 +0100

    feat: script con manipolazione del DOM

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,26 @@
 "use strict"
 
+const FizzBuzzList = document.getElementById("element-list");
+
 for (let counter = 1; counter <= 100; counter++) {
-
-
+    let FizzBuzzElement = document.createElement("li")
     if (counter % 3 === 0 && counter % 5 === 0) {
         console.log("FizzBuzz");
+        FizzBuzzElement.append("FizzBuzz");
+        FizzBuzzList.append(FizzBuzzElement);
     } else if (counter % 3 === 0) {
         console.log("Fizz");
+        FizzBuzzElement.append("Fizz");
+        FizzBuzzList.append(FizzBuzzElement);
     } else if (counter % 5 === 0) {
         console.log("Buzz");
+        FizzBuzzElement.append("Buzz");
+        FizzBuzzList.append(FizzBuzzElement);
     } else {
-        console.log(counter)
+        console.log(counter);
+        FizzBuzzElement.append(counter);
+        FizzBuzzList.append(FizzBuzzElement);
     }
 }
+
+
